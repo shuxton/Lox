c0de3e5142107189598da2a62a389557d9b18fd5
shuxton <shubhamsnj@gmail.com>
2020-10-11 15:12:45 +0530

    evaluating expressions using interpreter

--- src/com/shuxton/lox/Lox.java
+++ src/com/shuxton/lox/Lox.java
@@ -12,7 +12,9 @@ import java.util.List;
 
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -28,6 +30,7 @@ public class Lox {
       byte[] bytes=Files.readAllBytes(Paths.get(path));
       run(new String(bytes,Charset.defaultCharset()));
       if (hadError) System.exit(65);
+      if (hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException{
@@ -51,8 +54,7 @@ public class Lox {
       // Stop if there was a syntax error.
       if (hadError) return;
   
-      System.out.println(new AstPrinter().print(expression));
-  }
+      interpreter.interpret(expression);  }
 
   static void error(int line, String message){
       report(line,"",message);
@@ -69,5 +71,11 @@ public class Lox {
     } else {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 }
